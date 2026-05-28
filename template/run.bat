06bddc253299c01f69491dd85ac4c226f183153e
Taylor Barnes <taylor.a.barnes@gmail.com>
2026-05-28 10:13:42 -0500

    Split Containerfile into base and user-owned

--- template/run.bat
+++ template/run.bat
@@ -5,6 +5,7 @@ echo.
 
 set /p IMAGE=<.guardrails\podman\image_name
 
+podman build -t guardrails-base:latest .guardrails\podman
 podman build -t %IMAGE% .
 
 powershell -ExecutionPolicy Bypass -File .guardrails\interface.ps1 -Image %IMAGE%
--- template/run.sh
+++ template/run.sh
@@ -2,6 +2,7 @@
 
 IMAGE=$(cat .guardrails/podman/image_name)
 
+podman build -t guardrails-base:latest .guardrails/podman
 podman build -t "$IMAGE" .
 
 bash .guardrails/interface.sh "$IMAGE"
